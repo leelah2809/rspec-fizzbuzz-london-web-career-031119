54236990af5b8ed97a95a605f1a5a472aa639b42
Okunola Jelilat Dasola <jelilatokunola2809@gmail.com>
2019-02-24 20:36:27 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,13 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(no)
+  if (no % 15)  == 0
+  'FizzBuzz'
+   elsif (no % 5) == 0 
+     'Buzz'
+    elsif (no % 3) == 0
+   "Fizz"
+  else
+    puts 'nil'
+end
+end 
