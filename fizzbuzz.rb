d4dc45f5937afb3c882305a2153ddcd6f04e81d7
Okunola Jelilat Dasola <jelilatokunola2809@gmail.com>
2019-02-24 20:36:12 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,13 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(no)
-  if (no % 3) ! (no % 15) == 0
- 'Fizz'
+  if (no % 15)  == 0
+  'FizzBuzz'
    elsif (no % 5) == 0 
-   'Buzz'
-    elsif (no % 3) && (no % 5) == 0 
-   "FizzBuzz"
+     'Buzz'
+    elsif (no % 3) == 0
+   "Fizz"
   else
-     puts 'nil'
+    puts 'nil'
 end
-end
+end 
